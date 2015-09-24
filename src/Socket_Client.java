980968528958287df74ef8c21a63e006395aa246
Quentin Audinot <quentin.audinot@gmail.com>
2015-09-24 18:47:55 -0400

    TCP Socket Client done

--- src/Socket_Client.java
+++ src/Socket_Client.java
@@ -1,9 +1,73 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class Socket_Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		TCPClient client = new TCPClient("localhost", 8000);
+		
+		client.display();
 	}
+}
 
+class TCPClient {
+	Socket ClientSoc;
+	
+	DataInputStream din;
+	DataOutputStream dos;
+	ObjectOutputStream oos;
+	BufferedReader br;
+	
+	public TCPClient(String hostName, int port) {
+		try {
+			ClientSoc = new Socket(hostName, port);
+			din = new DataInputStream(ClientSoc.getInputStream());
+			dos = new DataOutputStream(ClientSoc.getOutputStream());
+			oos = new ObjectOutputStream(ClientSoc.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(System.in));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void display() throws Exception{
+		
+		String nombreClient1;
+		String nombreClient2;
+		String messageServeur;
+		
+		boolean quitter = false;
+		int choix = 0;
+		
+		while(!quitter) {
+			System.out.println("Bienvenue, vous voulez : \n");
+			System.out.println("	1 - Additionner deux nombres");
+			System.out.println("	2 - Quitter");
+			
+			choix = Integer.parseInt(br.readLine());
+			if (choix == 1) {
+				System.out.println("\nVeuillez rentrer le premier nombre : ");
+				nombreClient1 = br.readLine();
+				System.out.println("\nVeuillez rentrer le deuxième nombre : ");
+				nombreClient2 = br.readLine();
+				
+				Calc calcul = new Calc(nombreClient1, nombreClient2);
+				
+				oos.writeObject(calcul);
+				dos.writeUTF("add");
+				
+				messageServeur = din.readUTF();
+				System.out.println("\nRésultat : " + messageServeur);
+				
+			} else if (choix == 2) {
+				dos.close();
+				oos.close();
+				quitter = true;
+			}
+		}
+	}
 }
