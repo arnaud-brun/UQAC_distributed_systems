348a83bc330b9cebe44ed187cae0a410c7ce154d
Quentin Audinot <quentin.audinot@gmail.com>
2015-09-24 18:09:42 -0400

    Modification Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,18 @@
+import java.io.Serializable;
 
-public class Calc {
+public class Calc implements Serializable{
 
-	public int add(String a, String b) {
-		int x = Integer.parseInt(a);
-		int y = Integer.parseInt(b);
+	private String a;
+	private String b;
+	
+	public Calc(String a, String b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add() {
+		int x = Integer.parseInt(this.a);
+		int y = Integer.parseInt(this.b);
 		return x + y;
 	}
 }
--- src/Socket_Serveur.java
+++ src/Socket_Serveur.java
@@ -2,8 +2,7 @@
 public class Socket_Serveur {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
