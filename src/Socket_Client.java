3f715b0553c3c70b98c59271875bf1db5175023d
Arnaud <arnaud.brun.vidal@gmail.com>
2015-09-28 11:56:23 -0300

    Ajout d'une garde sur la saisie utilisateur : filtré sur uniquement des entiers valides

--- src/Socket_Client.java
+++ src/Socket_Client.java
@@ -1,19 +1,12 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Socket_Client {
-
-<<<<<<< HEAD
-	public static void main(String[] args) throws Exception{
-		TCPClient client = new TCPClient("localhost", 8000);
-		
-		client.display();
-	}
-=======
 
 	public static void main(String[] args) throws Exception{		
 		TCPClient client = new TCPClient("localhost", 8000);		
@@ -31,7 +24,7 @@ class TCPClient {
 	
 	public TCPClient(String hostName, int port) {
 		try {
-			//Instanciation of Socket and relative streams for Data IN / Out & Object Out
+			//Instantiation of Socket and relative streams for Data IN / Out & Object Out
 			ClientSoc = new Socket(hostName, port);
 			din = new DataInputStream(ClientSoc.getInputStream());
 			dos = new DataOutputStream(ClientSoc.getOutputStream());
@@ -56,21 +49,17 @@ class TCPClient {
 		int choix = 0;
 		
 		
-		System.out.println("Bienvenue, vous voulez : \n");
-		while(!quitter) {			
-			System.out.println("	1 - Additionner deux nombres");
-			System.out.println("	2 - Quitter");
+
+		while(!quitter) {
 			
-			choix = Integer.parseInt(br.readLine());
+			choix = Integer.parseInt(acceptOnlyInteger("Bienvenue, vous voulez : \n	1 - Additionner deux nombres\n	2 - Quitter\n"));
 			
 			//Will process a computation between to integers if Choix=1 or Quit the application if choix=2
 			if (choix == 1) {
 				
 				//User inputs
-				System.out.println("\nVeuillez rentrer le premier nombre : ");
-				nombreClient1 = br.readLine();
-				System.out.println("\nVeuillez rentrer le deuxième nombre : ");
-				nombreClient2 = br.readLine();
+				nombreClient1 = acceptOnlyInteger("\nVeuillez rentrer le premier nombre : ");
+				nombreClient2 = acceptOnlyInteger("\nVeuillez rentrer le second nombre : ");
 				
 				//Instanciate a Calc object with the 2 given inputs and write it to the server
 				Calc calcul = new Calc(nombreClient1, nombreClient2);				
@@ -90,63 +79,22 @@ class TCPClient {
 		ClientSoc.close();
 		System.out.println("-------------------------------------------------------");
 	}
->>>>>>> 84f6e240c2393b57eb70fd11e9d3565284db92a9
-}
+	
+	//Function to accept only correct Integer as user input
+	public String acceptOnlyInteger(String instruction){		
+		int validNumber;
+		String input;
+		
+		while(true){			
+			try{
+				System.out.println(instruction);
+				input = br.readLine();
+				validNumber = Integer.parseInt(input);
+				return input;
+			}catch(NumberFormatException | IOException e) {
+				System.out.println("\n /_!_\\ Veuillez rentrer un nombre entier, compris entre "+Integer.MIN_VALUE+" et "+Integer.MAX_VALUE+"");
+			}			
+		}
+	}
 
-class TCPClient {
-	Socket ClientSoc;
-	
-	DataInputStream din;
-	DataOutputStream dos;
-	ObjectOutputStream oos;
-	BufferedReader br;
-	
-	public TCPClient(String hostName, int port) {
-		try {
-			ClientSoc = new Socket(hostName, port);
-			din = new DataInputStream(ClientSoc.getInputStream());
-			dos = new DataOutputStream(ClientSoc.getOutputStream());
-			oos = new ObjectOutputStream(ClientSoc.getOutputStream());
-			br = new BufferedReader(new InputStreamReader(System.in));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void display() throws Exception{
-		
-		String nombreClient1;
-		String nombreClient2;
-		String messageServeur;
-		
-		boolean quitter = false;
-		int choix = 0;
-		
-		while(!quitter) {
-			System.out.println("Bienvenue, vous voulez : \n");
-			System.out.println("	1 - Additionner deux nombres");
-			System.out.println("	2 - Quitter");
-			
-			choix = Integer.parseInt(br.readLine());
-			if (choix == 1) {
-				System.out.println("\nVeuillez rentrer le premier nombre : ");
-				nombreClient1 = br.readLine();
-				System.out.println("\nVeuillez rentrer le deuxième nombre : ");
-				nombreClient2 = br.readLine();
-				
-				Calc calcul = new Calc(nombreClient1, nombreClient2);
-				
-				oos.writeObject(calcul);
-				dos.writeUTF("add");
-				
-				messageServeur = din.readUTF();
-				System.out.println("\nRésultat : " + messageServeur);
-				
-			} else if (choix == 2) {
-				dos.close();
-				oos.close();
-				quitter = true;
-			}
-		}
-	}
 }
